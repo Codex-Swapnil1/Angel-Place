cd8e2627f8d6cf42b78383e4d092d76dfffe191f
mohitd427 <mohitdon0013@gmail.com>
2023-01-19 00:36:37 +0530

    aadded day-2

--- Frontend/src/Routes/ALlRoutes.jsx
+++ Frontend/src/Routes/ALlRoutes.jsx
@@ -1,20 +1,32 @@
 import { Routes ,Route} from "react-router-dom";
+import MainLayout from "../Components/MainLayout";
+import AdminDashboard from "../Pages/AdminDashboard";
+import ForgotPassword from "../Pages/ForgotPassword";
 import Homepage from "../Pages/Homepage";
+import Login from "../Pages/Login";
+import ResetPassword from "../Pages/ResetPassword";
 import ProductPage from "../ProductPages_manmohan/ProductPage";
 
 
 export default function AllRoutes(){
 
 
-    return <Routes>
-<Route path="/product" element={<ProductPage/>} ></Route>
-<Route path="/" element={<Homepage/>} ></Route>
-<Route path="/product/:id" element={<h2>single product page</h2>}  ></Route>
-<Route path="/cart" element={<h1>cart page</h1>} ></Route>
-<Route path="/login" element={<h3>login page</h3>} ></Route>
-<Route path="/register" element={<h3>Signup page</h3>} ></Route>
-<Route path="/admin" element={<h3>admin page </h3>} ></Route>
-
-
-    </Routes>
+    return (
+      <Routes>
+        <Route path="/product" element={<ProductPage />}></Route>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route
+          path="/product/:id"
+          element={<h2>single product page</h2>}
+        ></Route>
+        <Route path="/cart" element={<h1>cart page</h1>}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<h3>Signup page</h3>}></Route>
+        <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+        <Route path="/reset-password" element={<ResetPassword />}></Route>
+        <Route path="/admin" element={<MainLayout />}>
+          <Route index element={<AdminDashboard/>}/>
+        </Route>
+      </Routes>
+    );
 }
