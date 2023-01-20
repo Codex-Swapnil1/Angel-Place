12d193ed26c8fd9484148ff610bc4a83841a3c30
raobaba <shrikishunr7@gmail.com>
2023-01-20 22:09:58 +0530

    login signup without css

--- Frontend/src/ProductPages_manmohan/ProductPage.jsx
+++ Frontend/src/ProductPages_manmohan/ProductPage.jsx
@@ -1,15 +1,15 @@
-import { useDispatch, useSelector } from "react-redux"
-import { getprData } from "../Redux/App/action"
-export default function ProductPage(){
-    const {log}=console
+// import { useDispatch, useSelector } from "react-redux"
+// import { getprData } from "../Redux/App/action"
+// export default function ProductPage(){
+//     const {log}=console
     
-    const data=useSelector((store)=>store.Appreducer)
+//     const data=useSelector((store)=>store.Appreducer)
 
-    console.log(data)
+//     console.log(data)
 
 
-    return <>
+//     return <>
     
      
-    </>
-}
+//     </>
+// }
--- Frontend/src/Routes/ALlRoutes.jsx
+++ Frontend/src/Routes/ALlRoutes.jsx
@@ -1,18 +1,19 @@
-import { Routes ,Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import ProductPage from "../ProductPages_manmohan/ProductPage";
+import Login from "../Components/Login.jsx";
+import Register from "../Components/Register.jsx";
 
-
-export default function AllRoutes(){
+export default function AllRoutes() {
 
 
     return <Routes>
-<Route path="/product" element={<ProductPage/>} ></Route>
-<Route path="/" element={<h1>Landing page</h1>} ></Route>
-<Route path="/product/:id" element={<h2>single product page</h2>}  ></Route>
-<Route path="/cart" element={<h1>cart page</h1>} ></Route>
-<Route path="/login" element={<h3>login page</h3>} ></Route>
-<Route path="/register" element={<h3>Signup page</h3>} ></Route>
-<Route path="/admin" element={<h3>admin page </h3>} ></Route>
+        <Route path="/product" element={<ProductPage />} ></Route>
+        <Route path="/" element={<h1>Landing page</h1>} ></Route>
+        <Route path="/product/:id" element={<h2>single product page</h2>}  ></Route>
+        <Route path="/cart" element={<h1>cart page</h1>} ></Route>
+        <Route path="/login" element={<Login />} ></Route>
+        <Route path="/register" element={<Register />} ></Route>
+        <Route path="/admin" element={<h3>admin page </h3>} ></Route>
 
 
     </Routes>
