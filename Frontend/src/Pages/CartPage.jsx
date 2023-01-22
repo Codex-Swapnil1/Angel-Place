a135c3e62e061479396c01e025b0482f9654cd9c
Codex-Swapnil1 <swapnilparchakewrd2@gmail.com>
2023-01-23 00:38:35 +0530

    responsive Cart

--- Frontend/src/Pages/CartPage.jsx
+++ Frontend/src/Pages/CartPage.jsx
@@ -1,12 +1,23 @@
-import { Box, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
+import { Box, Image, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
 import React from 'react'
+import { useSelector } from 'react-redux'
+import styled from 'styled-components'
 import AddressComponent from '../Components/Cart/AddressComponent'
 import AvailableOfferComponents from '../Components/Cart/AvailableOfferComponents'
-import ProductCart from '../Components/Cart/ProductCart'
+import ProductCart from '../Components/Cart/ProductCart';
+import logo from "../Resources/1.png"
 
 const CartPage = () => {
+  const cart = useSelector((store)=>store.Appreducer)
+  console.log(cart)
   return (
-    <div><Tabs size='md' variant='enclosed'>
+    <Wrapper>
+    <Box className='navcart'>
+      <Box >
+        <Image className='logoimg' src={logo} alt="logo" />
+      </Box> <Box>Login / Register</Box>
+    </Box>
+    <Tabs size='md' variant='enclosed'>
     <TabList>
       <Tab>Shopping Cart</Tab>
       <Tab>My Shortlist</Tab>
@@ -23,8 +34,21 @@ const CartPage = () => {
         <p>two!</p>
       </TabPanel>
     </TabPanels>
-  </Tabs></div>
+  </Tabs></Wrapper>
   )
 }
 
-export default CartPage
+export default CartPage;
+
+const Wrapper = styled.div`
+.navcart{
+  max-width:80%;
+  display:flex;
+  justify-content:space-between;
+  margin: auto;
+}
+.logoimg{
+  max-width: 110px;
+  border-radius : 5px
+}
+`
