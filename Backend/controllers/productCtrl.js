aa8ba17323f95de79e74f7d08832f230a4fe5d6f
Codex-Swapnil1 <swapnilparchakewrd2@gmail.com>
2023-01-22 14:07:19 +0530

    update

--- Backend/controllers/productCtrl.js
+++ Backend/controllers/productCtrl.js
@@ -1,4 +1,4 @@
-const Products = require('../Models/Product.Model')
+const Products = require("../Models/Product.Model");
 
 // Filter, sorting and paginating
 
@@ -49,66 +49,87 @@ class APIfeatures {
 }
 
 const productCtrl = {
-    getProducts: async(req, res) =>{
-        try {
-            const features = new APIfeatures(Products.find(), req.query)
-            .filtering().sorting().paginating()
+  getProducts: async (req, res) => {
+    try {
+      const features = new APIfeatures(Products.find(), req.query)
+        .filtering()
+        .sorting()
+        .paginating();
 
-            const products = await features.query
+      const products = await features.query;
 
-            res.json({
-                status: 'success',
-                result: products.length,
-                products: products
-            })
-
-        } catch (err) {
-            return res.status(500).json({msg: err.message})
-        }
-    },
-    createProduct: async(req, res) =>{
-        try {
-            const {product_id, title, price, description, content, images, category} = req.body;
-            if(!images) return res.status(400).json({msg: "No image upload"})
-
-            const product = await Products.findOne({product_id})
-            if(product)
-                return res.status(400).json({msg: "This product already exists."})
-
-            const newProduct = new Products({
-                product_id, title: title.toLowerCase(), price, description, content, images, category
-            })
-
-            await newProduct.save()
-            res.json({msg: "Created a product"})
-
-        } catch (err) {
-            return res.status(500).json({msg: err.message})
-        }
-    },
-    deleteProduct: async(req, res) =>{
-        try {
-            await Products.findByIdAndDelete(req.params.id)
-            res.json({msg: "Deleted a Product"})
-        } catch (err) {
-            return res.status(500).json({msg: err.message})
-        }
-    },
-    updateProduct: async(req, res) =>{
-        try {
-            const {title, price, description, content, images, category} = req.body;
-            if(!images) return res.status(400).json({msg: "No image upload"})
-
-            await Products.findOneAndUpdate({_id: req.params.id}, {
-                title: title.toLowerCase(), price, description, content, images, category
-            })
-
-            res.json({msg: "Updated a Product"})
-        } catch (err) {
-            return res.status(500).json({msg: err.message})
-        }
+      res.json({
+        status: "success",
+        result: products.length,
+        products: products,
+      });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
     }
-}
+  },
+  createProduct: async (req, res) => {
+    try {
+      const {
+        product_id,
+        title,
+        price,
+        description,
+        content,
+        images,
+        category,
+      } = req.body;
+      if (!images) return res.status(400).json({ msg: "No image upload" });
 
+      const product = await Products.findOne({ product_id });
+      if (product)
+        return res.status(400).json({ msg: "This product already exists." });
 
-module.exports = productCtrl
+      const newProduct = new Products({
+        product_id,
+        title: title.toLowerCase(),
+        price,
+        description,
+        content,
+        images,
+        category,
+      });
+
+      await newProduct.save();
+      res.json({ msg: "Created a product" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  deleteProduct: async (req, res) => {
+    try {
+      await Products.findByIdAndDelete(req.params.id);
+      res.json({ msg: "Deleted a Product" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  updateProduct: async (req, res) => {
+    try {
+      const { title, price, description, content, images, category } = req.body;
+      if (!images) return res.status(400).json({ msg: "No image upload" });
+
+      await Products.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          title: title.toLowerCase(),
+          price,
+          description,
+          content,
+          images,
+          category,
+        }
+      );
+
+      res.json({ msg: "Updated a Product" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+};
+
+module.exports = productCtrl;
